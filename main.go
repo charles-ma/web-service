1d5825c280e80e92c67a7bf0657aee7c2209179a
best droplet <mcrichard.buaa@gmail.com>
2020-12-30 07:07:33 +0000

    implement randome number generator

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Look what number you got!\n%d", rand.Intn(101))
 }
 
 func main() {
