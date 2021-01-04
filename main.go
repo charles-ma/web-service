c2e7186cf2ca0028b5b5fe9c7430567ee491c083
best droplet <mcrichard.buaa@gmail.com>
2021-01-04 05:04:15 +0000

    add random number method into math package

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
+	"web-service/mymath"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	n := rand.Intn(100) + 1
+	n := mymath.GenerateRandomNumber(100)
 	fmt.Fprintf(w, "Look what number you got from Docker!\n%d\n", n)
 
-	if n > 90 {
+	if n >= 90 {
 		fmt.Fprintf(w, "\nperfect!")
 	}
 }
--- mymath/myMath.go
+++ mymath/myMath.go
@@ -1,9 +1,17 @@
 package mymath
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 // PrintNum is a good method
 func PrintNum(x int) {
 	fmt.Println("this is a method in another package")
 	fmt.Println(x)
+}
+
+// GenerateRandomNumber ...
+func GenerateRandomNumber(limit int) int {
+	return rand.Intn(limit) + 1
 }
